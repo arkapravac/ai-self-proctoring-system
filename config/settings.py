77ebf22cf24fd47186728b9d0922915b608498b3
Arkaprava Chakraborty <arkapravac366@gmail.com>
2025-11-02 01:45:34 +0530

    Add violation settings to the Settings class

--- config/settings.py
+++ config/settings.py
@@ -3,4 +3,8 @@ class Settings:
     CAMERA_WIDTH = 1280
     CAMERA_HEIGHT = 720
     CAMERA_FPS = 30
+    # Violations
+    MAX_WARNINGS = 3
+    WARNING_COOLDOWN_SEC = 5
+    DETECTION_THRESHOLD_FRAMES = 3
    
