0cc8ff9d7c180dcf7439836dcce0925feb0d8a4c
Arkaprava Chakraborty <arkapravac366@gmail.com>
2025-11-03 00:50:17 +0530

    Implement camera setup method
    Added setup method for camera configuration.

--- hardware/camera.py
+++ hardware/camera.py
@@ -1,5 +1,15 @@
 import cv2
-lass Camera:
+class Camera:
     def __init__(self):
         self.cap = cv2.VideoCapture(0)
         self._setup()
+def _setup(self):
+        s = Settings
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH,s.CAMERA_WIDTH)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, s.CAMERA_HEIGHT)
+        self.cap.set(cv2.CAP_PROP_FPS,s.CAMERA_FPS)
+        self.cap.set(cv2.CAP_PROP_AUTOFOCUS, 1)
+        self.cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
+        self.cap.net(cv2.CAP_PROP_EXPOSURE, -4)
+        self.cap.net(cv2.CAP_PROP_BRIGHTNESS, 128)
+        self.cap.net(cv2.CAP_PROP_CONTRAST, 64)
