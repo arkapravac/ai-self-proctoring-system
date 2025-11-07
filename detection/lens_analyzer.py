f48147ea08ff67796b88ad60d53d1e64a9f4f553
Arkaprava Chakraborty <arkapravac366@gmail.com>
2025-11-07 11:23:51 +0530

    Add height, width, and landmark variables in analyze
    Extract height and width from the frame shape and define landmark variables for outer and iris points.

--- detection/lens_analyzer.py
+++ detection/lens_analyzer.py
@@ -5,3 +5,8 @@ class LensAnalyzer:
     def analyze(frame, landmarks):
         if not landmarks:
             return False
+         h, w = frame.shape[:2]
+        l_outer = landmarks.landmark[130]
+        r_outer = landmarks.landmark[359]
+        l_iris = landmarks.landmark[468]
+        r_iris = landmarks.landmark[473]
