458d9030c67c01279e362e455a92226e241b16f2
Arkaprava Chakraborty <arkapravac366@gmail.com>
2025-11-07 10:44:40 +0530

    Implement gaze tracking functionality
    Added method to check if the user is looking at the screen.

--- detection/gaze_tracker.py
+++ detection/gaze_tracker.py
@@ -10,3 +10,12 @@ class GazeTracker:
     def get_landmarks(selfs,rgb_frame):
         results.face_mesh.process(rbg_frame)
         return results.multi_face_landmarks[0] if results.multi_face_landmarks else None
+
+    def is_looking_at_screen(self, landmarks):
+        if not landmarks:
+            return False
+
+            l_iris_x = landmarks.landmark[468].x
+            r_iris_xr_iris_x = landmarks.landmark[473].x
+            l_left, l_right = landmarks.landmark[130].x, landmarks.landmark[133].x
+            r_left, r_right = landmarks.landmark[362].x,landmarks.landmark[263].x
