166c088da4238c2fb6f01e95fe3512a388dc4a2c
Arkaprava Chakraborty <arkapravac366@gmail.com>
2025-11-08 01:39:06 +0530

    Added get_roi function for region of interest calculation
    Implement get_roi function to define region based on outer and iris landmarks.

--- detection/lens_analyzer.py
+++ detection/lens_analyzer.py
@@ -10,3 +10,13 @@ class LensAnalyzer:
         r_outer = landmarks.landmark[359]
         l_iris = landmarks.landmark[468]
         r_iris = landmarks.landmark[473]
+
+def get_roi(outer,iris)
+        ox,oy = int(outer.x * w), int(outer.y * h)
+        ix,iy = int(iris.x * w), int(iris.y * h)
+        radius = int(abs(ix - ox)* 1.8)
+        x1 = max(0, ix - radius)
+        y1 = max(0, iy - radius)
+        x2 = min(w, ix + radius)
+        y2 = min(h, iy + radius)
+        return (x1,y1,x2,y2)
